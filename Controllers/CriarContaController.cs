1b4b78e9b55692668d2c2980c9ec8d634708a2a8
fernando <fernandoalanfillmann@gmail.com>
2023-09-27 17:31:39 -0300

    feat: finaliza controller de criacao de conta

--- Controllers/CriarContaController.cs
+++ Controllers/CriarContaController.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
 //Caminho da rota é o nome da Classe e suas chamadas interiores sem o "Controller"
 namespace recados_api
@@ -15,16 +11,16 @@ namespace recados_api
         [HttpPost]
         public IActionResult Post([FromBody] UsuarioModelo modelo)
         {
-            try{
+            try {
                 var response = new UsuarioModelo(
                     modelo.Username,
                     modelo.Senha,
                     Guid.NewGuid().ToString()
                 );
 
-            new CriarContaService().Service(response);
+                new CriarContaService().Service(response);
           
-            return Ok(response); 
+                return Ok(response); 
 
             } catch (ErroHTTP erro) {
                 return new CriaErroHTTP().MandarResposta(erro);
