831e31642e74bab5fd47a5ed25624bf06b7cdab9
fernando <fernandoalanfillmann@gmail.com>
2023-09-28 15:39:30 -0300

    refactor: alteracoes e testes

--- Controllers/EntrarContaController.cs
+++ Controllers/EntrarContaController.cs
@@ -1,23 +1,43 @@
-using System;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace recados_api
 {
     [ApiController]
-    [Route("[controller]")]
     public class EntrarContaController : ControllerBase
     {
-        [HttpGet]
-        public IActionResult Get([FromBody] UsuarioModelo modelo)
+        [Route("/login")]
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult Login([FromBody] UsuarioModelo modelo)
         {
             try{
-                string result = new EntrarContaService().Service(modelo);
+                object result = new EntrarContaService().Service(modelo);
 
                 return Ok(result);
                 
             }catch (ErroHTTP erro) {
                 return new CriaErroHTTP().MandarResposta(erro);
             }
+        }
+
+        [Route("/delete")]
+        [Authorize]
+        [HttpDelete]
+        public IActionResult Delete([FromBody] UsuarioModelo modelo)
+        {
+            try{
+                var token = GetUserId();
+                return Ok();
+                
+            }catch (ErroHTTP erro) {
+                return new CriaErroHTTP().MandarResposta(erro);
+            }
+        }
+        private string GetUserId()
+        {
+            return User.Claims.First(i => i.Type == "Id").Value;
         }
     }
 }
