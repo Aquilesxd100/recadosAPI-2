dc21d53f84a09255020abe8881f71a011b0be395
fernando <fernandoalanfillmann@gmail.com>
2023-09-28 11:24:09 -0300

    refactor: altera nome dos metodos de validacao

--- service/CriarContaService.cs
+++ service/CriarContaService.cs
@@ -5,7 +5,13 @@ namespace recados_api
     public class CriarContaService
     {
         public void Service(UsuarioModelo modelo){
-            new CriarContaValidator().Validator(modelo);
+            new UsuarioValidator(modelo.Username, modelo.Senha)
+                .CampoPreechido()
+                .CampoType()
+                .QntCaracteres()
+                .SenhaValid()
+                .CaracterInvalido()
+                .UsernameJaExiste();
 
             new UsuarioRepository().CriarConta(modelo);
         }
--- service/EntrarContaService.cs
+++ service/EntrarContaService.cs
@@ -3,7 +3,11 @@ namespace recados_api
     public class EntrarContaService
     {
         public string Service(UsuarioModelo modelo){
-            new EntrarContaValidator().Validator(modelo);
+            new UsuarioValidator(modelo.Username, modelo.Senha)
+                .CampoPreechido()
+                .CampoType()
+                .QntCaracteres()
+                .CaracterInvalido();
             return new UsuarioRepository().EntrarConta(modelo);
         }
     }
