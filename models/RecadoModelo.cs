b3a18a440ad08dab62a59121b706ab0b5e8f0081
fernando <fernandoalanfillmann@gmail.com>
2023-09-29 09:28:10 -0300

    feat: cria modelo de recado

---
+++ models/RecadoModelo.cs
@@ -0,0 +1,14 @@
+namespace recados_api
+{
+    public class RecadoModelo
+    {
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public string Data { get; set; }
+        public string Horario { get; set; }
+        public bool Arquivado { get; set; }
+        public string Id { get; set; }
+
+    };
+
+};
