8cd5de5ecffbbf8a9290d19dd62ba52fbcd5c30e
fernando <fernandoalanfillmann@gmail.com>
2023-09-29 09:29:12 -0300

    feat: cria service de atualizar conta

---
+++ service/Usuario/AtualizarContaService.cs
@@ -0,0 +1,18 @@
+namespace recados_api
+{
+    public class AtualizarContaService
+    {
+        public void Service(string userId, UsuarioModelo modelo){
+            new UsuarioValidator(modelo.Username, modelo.Senha)
+                .ValidUserToken(userId)
+                .PreencherCampoAtualizar()
+                .CampoTypeAtualizar()
+                .CaracterInvalido()
+                .QntCaracteres()
+                .SenhaValid()
+                .UsernameJaExiste();
+
+            new UsuarioRepository().AtualizarConta(userId, modelo);
+        }
+    }
+}
