7b362b1e1181b98d9b9285605c318e89e484d406
Lucas dos Santos <lucas.dos.santos0330@gmail.com>
2023-10-02 14:55:26 -0300

    service de arquivar recado

---
+++ service/Recados/ArquivaRecadoService.cs
@@ -0,0 +1,15 @@
+namespace recados_api
+{
+    public class ArquivarRecadoService{
+        public void Service(string userId, string recadoId){
+            new UsuarioValidator()
+                .ValidUserToken(userId);
+
+            new RecadoValidator()
+                .PertenceAUsuarioId(userId, recadoId);
+
+            new RecadoRepository()
+                .ArquivaRecado(recadoId);
+        }
+    }
+}
