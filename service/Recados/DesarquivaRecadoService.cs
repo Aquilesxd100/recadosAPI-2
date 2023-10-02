8063446a0ce78aad422d74d29c62d0927b3d44c7
Lucas dos Santos <lucas.dos.santos0330@gmail.com>
2023-10-02 14:55:39 -0300

    service de desarquivar recado

---
+++ service/Recados/DesarquivaRecadoService.cs
@@ -0,0 +1,15 @@
+namespace recados_api
+{
+    public class DesarquivaRecadoService{
+        public void Service(string userId, string recadoId){
+            new UsuarioValidator()
+                .ValidUserToken(userId);
+
+            new RecadoValidator()
+                .PertenceAUsuarioId(userId, recadoId);
+
+            new RecadoRepository()
+                .DesarquivaRecado(recadoId);
+        }
+    }
+}
