bba53bc2bddf6f341a77ef3664f038bcc73722b2
fernando <fernandoalanfillmann@gmail.com>
2023-10-11 11:20:28 -0300

    feat: verifica conexao com banco em todo metodo

--- repository/UsuarioRepository.cs
+++ repository/UsuarioRepository.cs
@@ -8,11 +8,13 @@ namespace recados_api
         private static readonly MySqlConnection conn = Database.conexao;
 
         public void CriarConta(UsuarioModelo usuarioInfos){
+            Database.VerificarConexao();
             Console.WriteLine("Connecting to MySQL... Criar Conta");
             string sql = $"INSERT INTO Usuario VALUES ('{usuarioInfos.Username}', '{usuarioInfos.Senha}', '{usuarioInfos.Id}')";
             new Database().ExecuteSql(sql);
         }
         public string EntrarConta(UsuarioModelo usuarioInfos){
+            Database.VerificarConexao();
             UsuarioModelo user = EncontrarUsuarioByUsername(usuarioInfos.Username);
 
             if(user.Username == null && user.Senha == null && user.Id == null || usuarioInfos.Senha != user.Senha){
@@ -23,11 +25,13 @@ namespace recados_api
             return token;
         }
         public void DeletarConta(string userId){
+            Database.VerificarConexao();
             Console.WriteLine("Connecting to MySQL... Deletar Conta");
             string sql = $"DELETE FROM Usuario WHERE Id = '{userId}'";
             new Database().ExecuteSql(sql);
         }
         public void AtualizarConta(string userId, UsuarioModelo modelo){
+            Database.VerificarConexao();
             UsuarioModelo user = EncontrarUsuarioById(userId);
             if(user.Id == null){
                 throw new ErroHTTP(404, "Nenhum Usuário com esse Id encontrado.");
@@ -43,6 +47,7 @@ namespace recados_api
             new Database().ExecuteSql(sql);
         }
         public static UsuarioModelo  EncontrarUsuarioByUsername(string Username){
+            Database.VerificarConexao();
             Console.WriteLine("Connecting to MySQL... Encontrar Usuario By Username");
             string sql = $"SELECT * FROM Usuario WHERE Username = '{Username}'";
             MySqlCommand cmd = new MySqlCommand(sql, conn);
@@ -61,6 +66,7 @@ namespace recados_api
         }
 
         public static UsuarioModelo EncontrarUsuarioById(string Id){
+            Database.VerificarConexao();
             Console.WriteLine("Connecting to MySQL... Encontrar Usuario By Id");
             string sql = $"SELECT * FROM Usuario WHERE Id = '{Id}'";
             MySqlCommand cmd = new MySqlCommand(sql, conn);
