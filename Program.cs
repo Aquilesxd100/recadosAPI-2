a1bf646c6bfadc34589799710204cd65f54e6bcd
fernando <fernandoalanfillmann@gmail.com>
2023-10-11 11:18:59 -0300

    refactor: limpa codigo e remove abertura conexao

--- Program.cs
+++ Program.cs
@@ -8,18 +8,13 @@ namespace recados_api
     {
         public static void Main(string[] args)
         {
-           
-
             CreateHostBuilder(args).Build().Run();
-
-            // Database.conexao.Close();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    Database.AbrirConexao();
                     webBuilder.UseStartup<Startup>();
                 });
     }
